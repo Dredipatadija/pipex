7970eb64c062de01c457a7cd61a0d2af83206d59
Dredipatadija <andrearmra@gmail.com>
2024-12-13 22:21:27 +0100

    he hecho el pipe 2

--- errors.c
+++ errors.c
@@ -1,14 +1,35 @@
 #include "pipex.h"
 
+void	ft_free(char **cmd)
+{
+	int	i;
+
+	i = 0;
+	while (cmd[i] != NULL)
+	{
+		free(cmd[i]);
+		i++;
+	}
+	free(cmd);
+}
+
 void	ft_error1(char *str)
 {
 	perror(str);
 	exit(1);
 }
 
-void	ft_error2(char *str, int fd)
+void	ft_error2(char *str, int infd, int outfd)
 {
 	perror(str);
-	close(fd);
+	close(infd, outfd);
+	exit(1);
+}
+
+void	ft_error3(char *str, char **cmd, int infd, int outfd)
+{
+	perror(str);
+	close(infd, outfd);
+	ft_free(cmd);
 	exit(1);
 }
--- pipex.c
+++ pipex.c
@@ -1,5 +1,21 @@
 #include "pipex.h"
 
+void	ft_pipe2(char *cmd, int *pipefd, char **envp, int outfd, int infd)
+{
+	char	**s_cmd;
+
+	close(pipefd[1]);
+	if (dup2(outfd, 1) == -1)
+		ft_error2("Error: Dup2 failed at the output\n", infd, outfd);
+	close(outfd);
+	if (dup2(pipefd[0], 0) == -1)
+		ft_error2("Error: Dup2 failed at the input\n", infd, outfd);
+	close(pipefd[0]);
+	s_cmd = ft_split(cmd, ' ');
+	if (execve(s_cmd[0], s_cmd, envp) == -1)
+		ft_error3("Command not found", s_cmd, infd, outfd);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	int	pipefd[2];
@@ -11,16 +27,15 @@ int	main(int argc, char **argv, char **envp)
 	if (pipe(pipefd) == -1)
 		ft_error1("Error: Pipe failed\n");
 	infd = ft_openin(argv[1]);
+	outfd = ft_openout(argv[4]);
 	if (infd > 0)
 	{
 		if (dup2(infd, 0) == -1)
-			ft_error2("Error: Dup2 failed\n", infd);
+			ft_error2("Error: Dup2 failed\n", infd, outfd);
 		close(infd);
+		ft_pipes(argv, pipefd, envp, outfd, infd);
 	}
-	outfd = ft_openout(argv[4]);
-	if (infd == -1)
-		ft_pipe2(argv[3], pipefd, envp, outfd);
-	else
-		ft_pipes(argv, pipefd, envp, outfd);
+	else if (infd == -1)
+		ft_pipe2(argv[3], pipefd, envp, outfd, infd);
 	return (0);
 }
