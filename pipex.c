28fa440b56b727bfca0c8d6b1fac2337e5a0a868
Dredipatadija <andrearmra@gmail.com>
2025-01-14 13:33:05 +0100

    mejor entendido

--- pipex.c
+++ pipex.c
@@ -38,44 +38,43 @@ char	*ft_getpath(char *cmd, char **envp)
 	return (path);
 }
 
-void	ft_pipes(char **argv, int *pipefd, char **envp, int outfd)
+void	ft_parent(char **argv, int *pipefd, char **envp, int outfd)
 {
-	pid_t	pid;
-	pid_t	pid_two;
+	pid_t	pid1;
+	pid_t	pid2;
 
-	pid = fork();
-	if (pid == -1)
-		exit(1);
-	if (!pid)
-		ft_pipe1(argv, pipefd, envp, outfd);
+	pid1 = fork();
+	if (pid1 == -1)
+		MENSAJE DE ERROR Y EXIT
+	if (pid1 == 0)
+		ft_child1(argv, pipefd, envp, outfd);
 	else
 	{
-		pid_two = fork();
-		if (pid_two == -1)
-			exit(1);
+		pid2 = fork();
+		if (pid2 == -1)
+			MENSAJE DE ERROR Y EXIT
 		close(pipefd[1]);
-		if (!pid_two)
-			ft_pipe2(argv, pipefd, envp, outfd);
+		if (pid2 == 0)
+			ft_child2(argv, pipefd, envp, outfd);
 		else
 		{
-			close_fds(pipefd, outfd);
-			waitpid(pid, NULL, 0);
-			waitpid(pid_two, NULL, 0);
+			close_fds(pipefd[0], outfd);
+			waitpid(pid1, NULL, 0);
+			waitpid(pid2, NULL, 0);
 		}
 	}
 }
 
-void	ft_pipe1(char **argv, int *pipefd, char **envp, int outfd)
+void	ft_child1(char **argv, int *pipefd, char **envp, int outfd)
 {
 	char	**s_cmd;
 	char	*path;
 
 	close(pipefd[0]);
-	close(outfd);
 	if (dup2(pipefd[1], 1) == -1)
 		ft_error4("Error: Dup2 failed at the output\n", outfd);
 	close(pipefd[1]);
-	s_cmd = ft_split(argv[3], ' ');
+	s_cmd = ft_split(argv[2], ' ');
 	if (s_cmd == NULL || s_cmd[0] == NULL)
 		ft_error3("Error: Command not found", s_cmd, outfd);
 	if (s_cmd[0][0] != '.' && s_cmd[0][0] != '/')
@@ -91,7 +90,7 @@ void	ft_pipe1(char **argv, int *pipefd, char **envp, int outfd)
 		ft_error5("Error: Command not found\n", s_cmd);
 }
 
-void	ft_pipe2(char *cmd, int *pipefd, char **envp, int outfd)
+void	ft_child2(char *cmd, int *pipefd, char **envp, int outfd)
 {
 	char	**s_cmd;
 	char	*path;
@@ -130,15 +129,15 @@ int	main(int argc, char **argv, char **envp)
 	if (pipe(pipefd) == -1)
 		ft_error1("Error: Pipe failed\n");
 	infd = ft_openin(argv[1]);
-	outfd = ft_openout(argv[4]);
+	outfd = ft_openout(argv[ft_strlen(argv) - 1]);
 	if (infd > 0)
 	{
 		if (dup2(infd, 0) == -1)
 			ft_error2("Error: Dup2 failed\n", infd, outfd);
 		close(infd);
-		ft_pipes(argv, pipefd, envp, outfd);
+		ft_parent(argv, pipefd, envp, outfd);
 	}
 	else if (infd == -1)
-		ft_pipe2(argv[3], pipefd, envp, outfd);
+		ft_child2(argv[3], pipefd, envp, outfd);
 	return (0);
 }
